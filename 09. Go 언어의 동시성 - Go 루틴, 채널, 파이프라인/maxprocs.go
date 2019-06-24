84b911eb61f2cc5bf69cb57afb9206538c99e9d1
msnodeve <msnodeve@gmail.com>
2019-06-24 21:22:02 +0900

    MAX PROCESS

--- 09. Go 언어의 동시성 - Go 루틴, 채널, 파이프라인/maxprocs.go
+++ 09. Go 언어의 동시성 - Go 루틴, 채널, 파이프라인/maxprocs.go
@@ -1,1 +1,14 @@
-// test code
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func getGOMAXPROCS() int {
+	return runtime.GOMAXPROCS(0)
+}
+
+func main() {
+	fmt.Printf("GOMAXPROCS: %d\n", getGOMAXPROCS())
+}
